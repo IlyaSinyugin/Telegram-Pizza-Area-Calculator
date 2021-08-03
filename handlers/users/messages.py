e5c65190b0c63767b625dfbf7be43841044440bb
Ilya <ilya@sinyugin.ru>
2021-08-03 13:54:34 +0200

    renamed variables

--- handlers/users/messages.py
+++ handlers/users/messages.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # Only messages with digits in them will be passed here
 @dp.message_handler(regexp='^[0-9]*$')
-async def bot_echo(message: types.Message):
+async def bot_area(message: types.Message):
     diameter = message.text
     area = np.pi * (int(diameter)/2) * (int(diameter)/2)
     await message.answer(f"The diameter you entered is {diameter}cm\n"
@@ -16,6 +16,6 @@ async def bot_echo(message: types.Message):
 
 # Handler that activates if the first one did not pass through
 @dp.message_handler()
-async def bot_echo_all(message: types.Message):
+async def bot_wrongFormat(message: types.Message):
     await message.answer(f"Wrong format!"
                          f"Please, input digits")
