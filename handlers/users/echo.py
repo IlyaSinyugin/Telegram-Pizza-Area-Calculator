aedd9652a29fc3c1c78ceb78a37ee4a07edebace
Ilya <ilya@sinyugin.ru>
2021-08-03 13:08:37 +0200

    calculates area

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,14 +2,16 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from loader import dp
+import numpy as np
 
 
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния."
-                         f"Сообщение:\n"
-                         f"{message.text}")
+    diameter = message.text
+    area = np.pi * (int(diameter)/2) * (int(diameter)/2)
+    await message.answer(f"The diameter you entered is {diameter}\n"
+                         f"The area is {area}")
 
 
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,4 +6,5 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!")
+    await message.answer(f"Hello, {message.from_user.full_name}!\n"
+                         "Please, enter the diameter of the pizza in cm")
