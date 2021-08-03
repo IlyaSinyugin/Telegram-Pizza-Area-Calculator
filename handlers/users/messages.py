bcf7c99dba12208e2d3f9b1337b75fa0c301302a
Ilya <ilya@sinyugin.ru>
2021-08-03 13:31:54 +0200

    wrong format handler

---
+++ handlers/users/messages.py
@@ -0,0 +1,21 @@
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+from loader import dp
+import numpy as np
+
+
+# Only messages with digits in them will be passed here
+@dp.message_handler(regexp='^[0-9]*$')
+async def bot_echo(message: types.Message):
+    diameter = message.text
+    area = np.pi * (int(diameter)/2) * (int(diameter)/2)
+    await message.answer(f"The diameter you entered is {diameter}cm\n"
+                         f"The area is {int(area)}cm^2")
+
+
+# Handler that activates if the first one did not pass through
+@dp.message_handler()
+async def bot_echo_all(message: types.Message):
+    await message.answer(f"Wrong format!"
+                         f"Please, input digits")
